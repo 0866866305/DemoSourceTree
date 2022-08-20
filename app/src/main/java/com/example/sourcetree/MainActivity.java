6a7866e4003a76f5496f863e453e850e819aebf3
tuannghia16 <68336982+0866866305@users.noreply.github.com>
2022-08-20 15:39:20 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/sourcetree/MainActivity.java
+++ app/src/main/java/com/example/sourcetree/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Helllo", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "yes", Toast.LENGTH_SHORT).show();
                 Toast.makeText(this, "No", Toast.LENGTH_SHORT).show();
 
